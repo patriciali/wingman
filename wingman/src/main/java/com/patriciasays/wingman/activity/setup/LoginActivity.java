f2e33ab1d5d376f414445cec2a5db78aecb78d87
Patricia Li <patricia.li@gmail.com>
2015-02-13 21:43:08 -0800

    auth webview doesn't work; pushing anyway #yolo #18

--- wingman/src/main/java/com/patriciasays/wingman/activity/setup/LoginActivity.java
+++ wingman/src/main/java/com/patriciasays/wingman/activity/setup/LoginActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.patriciasays.wingman.R;
@@ -17,6 +20,26 @@ public class LoginActivity extends Activity {
 
     private EditText mUsernameView;
     private EditText mPasswordView;
+    private Button mLoginButton;
+    private WebView mGetTokenWebView;
+
+    public class CCMJavascriptInterface {
+
+        private SharedPreferences mJsSharedPreferences;
+
+        public CCMJavascriptInterface(SharedPreferences sharedPref) {
+            mJsSharedPreferences = sharedPref;
+        }
+
+        public void jsSetToken(String token) {
+            setToken(token);
+        }
+
+        public void log(String str) {
+            Log.d("AndroidFunction.log_fuck", str);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +49,33 @@ public class LoginActivity extends Activity {
 
         mUsernameView = (EditText) findViewById(R.id.username_textview);
         mPasswordView = (EditText) findViewById(R.id.password_textview);
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mGetTokenWebView = (WebView) findViewById(R.id.get_token_webview);
 
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String url = mSharedPreferences.getString(
+                Constants.URL_PREFERENCE_KEY, Constants.DEFAULT_SERVER_URL);
+        mGetTokenWebView.loadUrl(url);
+        mGetTokenWebView.getSettings().setJavaScriptEnabled(true);
+        mGetTokenWebView.addJavascriptInterface(
+                new CCMJavascriptInterface(mSharedPreferences), "AndroidFunction");
+        mGetTokenWebView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                mLoginButton.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     public void login(View view) {
-        // TODO WEBVIEW
-        setToken("zk8HiOoe7Pn8LM_0tMDm3IuT9N6pIofkJtH4qrarl3j");
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        String js = String.format(Constants.GET_AUTH_TOKEN_JS, username, password);
+        mGetTokenWebView.loadUrl("javascript:" + js);
     }
 
-    private void setToken(String token) {
+    private synchronized void setToken(String token) {
         SharedPreferences.Editor editor = mSharedPreferences.edit();
         editor.putString(Constants.AUTH_TOKEN_PREFERENCE_KEY, token);
         editor.commit();
--- wingman/src/main/java/com/patriciasays/wingman/util/Constants.java
+++ wingman/src/main/java/com/patriciasays/wingman/util/Constants.java
@@ -3,6 +3,14 @@ package com.patriciasays.wingman.util;
 public class Constants {
 
     /**
+     * Javascript script to send login
+     * email, password
+     */
+    public static final String GET_AUTH_TOKEN_JS =
+            "Meteor.loginWithPassword('%s', '%s', function(err) { if(err) { throw err; } " +
+                    "AndroidFunction.jsSetToken(Accounts._storedLoginToken()); })";
+
+    /**
      * Key names for storing information in SharedPreferences
      */
     public static final String URL_PREFERENCE_KEY = "url_key";
@@ -14,20 +22,20 @@ public class Constants {
     /**
      * Default server values
      */
-    public static final String DEFAULT_SERVER_URL = "http://staging.live.jflei.com/api/v0";
+    public static final String DEFAULT_SERVER_URL = "http://staging.live.jflei.com/api";
     public static final String DEFAULT_PORT = "80";
 
     /**
      * Server API endpoints
      */
-    public static final String COMPETITIONS_URL_SUFFIX = "/competitions";
+    public static final String COMPETITIONS_URL_SUFFIX = "/v0/competitions";
     // competition _id
-    public static final String ROUNDS_URL_SUFFIX = "/competitions/%s/rounds";
+    public static final String ROUNDS_URL_SUFFIX = "/v0/competitions/%s/rounds";
     // competition _id
     public static final String PARTICIPANTS_URL_SUFFIX =
-            "/competitions/%s/registrations";
+            "/v0/competitions/%s/registrations";
     // competition _id, eventCode, nthRound, token
     public static final String UPLOAD_TIME_URL_SUFFIX =
-            "/competitions/%s/rounds/%s/%d/results?token=%s";
+            "/v0/competitions/%s/rounds/%s/%d/results?token=%s";
 
 }
